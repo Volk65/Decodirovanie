242ef0ba8267db72d1595a8c3bef923d7f3c1c2b
user5-7 <user5-7@citp.energypk.ru>
2021-12-09 13:20:12 +0300

    Добавьте файлы проекта.

---
+++ Decodirovanie/Decodirovanie.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "RUS");
+    char buff[50] = { 0 };
+    cout << "Введите сообщение,которое будет закодированно" << endl;
+    cin.getline(buff, 49);
+    for (int i = 0; i < strlen(buff); i++)
+        buff[i] += 3;
+    cout << buff << endl;
+    cout << "Декодированное: " << endl;
+    for (int i = 0; i < strlen(buff); i++)
+        buff[i] -= 3;
+    cout << buff << endl;
+    for (int i = 0; i < strlen(buff); i++)
+        buff[i] += 3;
+    {ofstream out;          // поток для записи
+    out.open("D:\\Результат.txt"); // окрываем файл для записи
+    if (out.is_open())
+    {
+        out << buff << std::endl;
+    }
+    system("pause");
+    return 0;
+    }
+}
